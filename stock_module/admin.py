df911ac81ea06883b70e0a8167244581fd656958
SaeedAkbari191 <akbari.saeed.191@gmail.com>
2025-11-09 11:18:27 +0430

    inventory

--- stock_module/admin.py
+++ stock_module/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from stock_module.models import Stock
+from stock_module.models import Stock, StockLedger
 
 # Register your models here.
 
 
 admin.site.register(Stock)
+admin.site.register(StockLedger)
